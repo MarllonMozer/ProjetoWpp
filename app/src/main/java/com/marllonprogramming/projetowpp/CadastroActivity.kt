399ee4c4983e280f0e59f9c6bb9940b8be80acc5
MarllonDKT <marllonprogramming@gmail.com>
2024-09-17 17:01:07 -0300

    criando interface da tela de login e de cadastro

---
+++ app/src/main/java/com/marllonprogramming/projetowpp/CadastroActivity.kt
@@ -0,0 +1,30 @@
+package com.marllonprogramming.projetowpp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.marllonprogramming.projetowpp.databinding.ActivityCadastroBinding
+
+class CadastroActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityCadastroBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+
+        inicializarToolbar()
+
+    }
+
+    private fun inicializarToolbar() {
+        val toolbar = binding.includeToolbar.tbPrincipal
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = "Faça o seu cadastro"
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+}
